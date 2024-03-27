099f4a4a559b93d568e9c3decffc180d8e24452d
neickow <133252017+neickow@users.noreply.github.com>
2024-03-27 09:27:20 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,34 @@
+f=open('students.csv', encoding='UTF-8')
+a=f.readlines()
+f.close()
+a=[x.strip().split(',') for x in a]
+shapka=a.pop(0)
+for i in range(len(a)):
+    if 'Хадаров Владимир' in a[i][1]:
+        print(f'Ты получил: {a[i][-1]}, за проект - {a[i][2]}')
+        break
+d={}
+for x in a:
+    clas = x[3]
+    score=x[-1]
+    if score!='None':
+        if clas not in d:
+            d[clas]=[int(score)]
+        else:
+            d[clas]+=[int(score)]
+for clas in d:
+    d[clas] = round(sum(d[clas])/len(d[clas]), 3)
+for i in range(len(a)):
+    if a[i][-1]=='None':
+        a[i][-1]=str(d[a[i][3]])
+f=open('students_new.csv', 'w', encoding='UTF-8')
+f.write(str(shapka)+'\n')
+for x in a:
+    f.write(','.join(x)+'\n')
+f.close()
+# srr=0
+# for i in range(len(a)):
+#     if a[i][-1]!='None':
+#         sr.append(int(a[i][-1]))
+# print(sr)
+
